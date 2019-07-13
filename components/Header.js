7aa9580c9a014c745d96d33e7009d5fdf5452cec
Jack Rose <jackderryrose@gmail.com>
2019-07-13 17:58:28 +1000

    initial commit based on example

---
+++ components/Header.js
@@ -0,0 +1,7 @@
+import Link from 'next/link'
+
+const Header = () => (
+  <div>header</div>
+)
+
+export default Header
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import App from '../components/App'
+import Header from '../components/Header'
+
+export default () => (
+  <App>
+    <Header />
+  </App>
+)
