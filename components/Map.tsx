2b0a9fb3129c921a54f4af275d2c3961931cf0d9
Jack Rose <jackderryrose@gmail.com>
2019-07-14 19:06:56 +1000

    switch ubers mapbox out for standard

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import ReactMapGL, { ViewState } from 'react-map-gl';
+import mapboxgl, { Map as TMap } from 'mapbox-gl'
 
 import { BusinessListQueryComponent, BusinessListQueryQuery } from '../generated/graphql';
 
 import { LoadingGuard } from './LoadingGuard';
 
-const initialViewState: ViewState = {
-  // melbourne for now
-  latitude: -37.808163434,
-  longitude: 144.957829502,
-  zoom: 8
-};
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFja2RlcnJ5cm9zZSIsImEiOiJjankybGNkNWQwcHVpM2JvOXdmNzNvc2FlIn0.ahjk12cEB4FAaRR7IEXBag';
 
 const mapStyle = {
   version: 8,
@@ -42,18 +37,67 @@ type Props = {
   data: BusinessListQueryQuery;
 };
 
-const MapView = ({ data }: Props) => {
-  const [viewport, setViewport] = React.useState<ViewState>(initialViewState);
-  const dimentions = { height: 400, width: 400 };
+type State = {
+  lat: number;
+  lng: number;
+  zoom: number;
+};
 
-  return (
-    <ReactMapGL
-      {...viewport}
-      {...dimentions}
-      // mapStyle={mapStyle}
-      onViewportChange={(v) => setViewport(v)}
-    />
-  )
+const initialState: State = {
+  // melbourne for now
+  lat: -37.808163434,
+  lng: 144.957829502,
+  zoom: 8
+};
+
+class MapView extends React.Component<Props, State> {
+  private mapRef: React.RefObject<HTMLDivElement>;
+  private map: TMap | undefined;
+
+  constructor(props: Props) {
+    super(props);
+    this.state = initialState;
+    this.mapRef = React.createRef();
+  }
+
+  componentDidMount = () => {
+    if (!this.mapRef.current) return;
+
+    const { lng, lat, zoom } = this.state;
+
+    this.map = new mapboxgl.Map({
+      container: this.mapRef.current,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [lng, lat],
+      zoom
+    });
+
+    this.map.on('move', () => {
+      if (!this.map) return;
+
+      const { lng, lat } = this.map.getCenter();
+      this.setState({
+        lng: lng,
+        lat: lat,
+        zoom: this.map.getZoom()
+      });
+    });
+  }
+
+  render = () => {
+    return (
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          width: '100%'
+        }}
+        ref={this.mapRef}
+        className="absolute top right left bottom"
+      />
+    )
+  }
 }
 
 export const Map = () => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
+import dynamic from 'next/dynamic';
+
 import { App } from '../components/App'
-import { Map } from '../components/Map'
 import { Header } from '../components/Header'
+
+// mapbox gl crashes server side -> can't even be imported
+const DynamicMap = dynamic(() => import('../components/Map').then(mod => mod.Map as any), {
+  ssr: false,
+});
 
 export default () => (
   <App>
     <Header />
-    <Map />
+    <DynamicMap />
   </App>
 );
 
